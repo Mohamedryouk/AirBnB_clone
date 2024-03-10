f2c1ec74f01d314114d19888fdd0053bfb67b9f4
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-03-10 14:54:35 +0300

    done

--- models/base_model.py
+++ models/base_model.py
@@ -8,21 +8,21 @@ class BaseModel():
     BaseModel class for all models
     """
     def __init__(self, *args, **kwargs):
-            """
-            constructor or new instance made
-            """
-            if kwargs:
-                if '__class__' in kwargs:
-                    del kwargs['__class__']
-                for key, value in kwargs.items():
-                    if key == 'created_at' or key == 'updated_at':
-                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-                    else:
-                        setattr(self, key, value)
-            else:
-                self.id = str(uuid.uuid4())
-                self.created_at = datetime.utcnow()
-                self.updated_at = datetime.utcnow()
+        """
+        initialize the model
+        """
+        if kwargs:
+            if '__class__' in kwargs:
+                del kwargs['__class__']
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.isoformat())
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def save(self):
         """
@@ -37,7 +37,6 @@ class BaseModel():
         """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
-        #obj_dict['id'] = str(self.id)
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
